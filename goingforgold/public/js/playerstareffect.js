211cf49e8909a537760c46da364653ab09636305
Yassine <157384941+yassine-AO@users.noreply.github.com>
2024-06-04 13:58:36 +0100

    Add files via upload

---
+++ goingforgold/public/js/playerstareffect.js
@@ -0,0 +1,20 @@
+//Stars effect
+function stars() {
+    let e = document.createElement('div');
+    e.setAttribute('class', 'star');
+    document.body.appendChild(e);
+    e.style.left = Math.random() * + innerWidth + 'px';
+
+    let size = Math.random() * 12;
+    let duration = Math.random() * 5;
+
+    e.style.fontSize = 12 + 'px';
+    e.style.animationDuration = 2 + duration + 's';
+    setTimeout(function () {
+        document.body.removeChild(e);
+    }, 5000);
+}
+
+setInterval(function () {
+    stars()
+}, 100);
